e1a79ef1fed667c0b598e2fd9ed03cbc00a7bd61
Salamdi <dinislam91@gmail.com>
2022-08-30 17:02:53 +0100

    lint code

--- src/index.js
+++ src/index.js
@@ -5,11 +5,9 @@ import path from 'path';
 
 const dashifySymbols = (name) => `${name.replace(/https?:\/\//, '').replace(/[^\w\d]/ig, '-')}`;
 
-export default (url, outdir) => {
-  return axios.get(url)
-    .then(({ data }) => {
-      const fileName = `${dashifySymbols(url)}.html`;
-      const filePath = path.join(outdir, fileName);
-      return fs.writeFile(filePath, data);
-    });
-};
+export default (url, outdir) => axios.get(url)
+  .then(({ data }) => {
+    const fileName = `${dashifySymbols(url)}.html`;
+    const filePath = path.join(outdir, fileName);
+    return fs.writeFile(filePath, data);
+  });
